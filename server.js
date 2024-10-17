d1fe1ce82533d82ea3fe42613f99b466dcc64ecc
Mahesh-1503 <mnopxyz090@gmail.com>
2024-10-17 23:37:20 +0530

    set-2

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv');
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js'; 
 
 //dotenv config
 dotenv.config();
 
+//mongodb connection
+connectDB();
+
 //resr object
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get("/", (req, res) => {
